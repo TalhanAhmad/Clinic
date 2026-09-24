63f1f5eb48c4a9cb663901c56452dbabb3f00fb6
Talha ahmad awan <zablerkhanahmad@gmail.com>
2026-09-24 17:47:51 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,3 +9,4 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <html lang="en"><body>{children}</body></html>;
 }
+isSupercute()
